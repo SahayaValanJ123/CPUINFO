a94c7e073213eb893520a3a6112a5c3bd6022119
Sahaya Valan j <sahayavalanj1@gmail.com>
2024-02-18 13:38:58 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+# with open('requirements.txt', 'r') as f:
+#     requers = f.read().splitlines()
+# reqs=os.popen('pipreqs cpuinfo').read().splitlines()
+
+with open("README.md", "r") as f:
+    ld = f.read()
+
+reqs=['pyfiglet','psutils','OptionParser','setuptools']
+
+setup(
+    name="minicpuinfo",
+    version="2.2.0",
+    author='sahayavalan',
+    author_email='sahayavalanj1@gmail.com',
+    description='minicpu is cpuinfo mini version tool',
+    packages=find_packages(),
+    long_description=ld,
+    long_description_content_type='text/markdown',
+    install_requires=reqs,
+
+    entry_points={
+        'console_scripts': ['minicpuinfo=cpuinfo.minicpuinfo:main',],
+    },
+)
